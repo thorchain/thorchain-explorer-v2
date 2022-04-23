792ca81d0a4f30c5e7b971852b4cb8e56b177026
HooriRn <hoorirn@protonmail.com>
2022-04-23 12:07:47 +0430

    clickable address in vaults page

--- mixins.js/global.js
+++ mixins.js/global.js
@@ -9,5 +9,14 @@ export default {
         return require('~/assets/images/unknown.png');
       }
     },
+    gotoAddr(address) {
+      this.$router.push({ path: `/address/${address}` })
+    },
+    gotoNode(signer) {
+      this.$router.push({path: `/node/${signer}`});
+    },
+    gotoPool(pool) {
+      this.$router.push({ path: `/pool/${pool}`});
+    },
   }
 }
