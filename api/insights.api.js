6a8eb1fd0eeaa30fd87ac240837b195a8d3646ba
HooriRn <hoorirn@protonmail.com>
2022-07-19 09:02:35 +0100

    Add rune price history in Insight page

--- api/insights.api.js
+++ api/insights.api.js
@@ -10,3 +10,7 @@ export function getChurnHistory() {
 export function getFlipTVL() {
   return $axiosInstace.get(INSIGHT_URL + `ed73580a-3555-4750-b1fa-03d3c6bf59e3/data/latest`);
 }
+
+export function getRunePrice() {
+  return $axiosInstace.get(INSIGHT_URL + `1aaa2137-b392-40a1-a9ce-22512f02d722/data/latest`);
+}
