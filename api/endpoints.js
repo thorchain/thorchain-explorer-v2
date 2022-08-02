567af5af1543b65a43cb1c84bafbdc1035dee815
HooriRn <hoorirn@protonmail.com>
2022-08-02 22:10:34 +0430

    add middleware server for testnet & stagenet

--- api/endpoints.js
+++ api/endpoints.js
@@ -11,13 +11,13 @@ module.exports = {
     MIDGARD_GRAPH_QL: "https://stagenet-midgard.ninerealms.com/v2",
     THORNODE_URL: "https://stagenet-thornode.ninerealms.com/",
     TENDERMINT_URL: "https://stagenet-rpc.ninerealms.com/",
-    SERVER_URL: ""
+    SERVER_URL: "https://vanaheimex.com/stage/"
   },
   testnet: {
     MIDGARD_BASE_URL: "https://testnet.midgard.thorchain.info/v2/",
     MIDGARD_GRAPH_QL: "https://testnet.midgard.thorchain.info/v2",
     THORNODE_URL: "https://testnet.thornode.thorchain.info/",
     TENDERMINT_URL: "https://testnet.rpc.thorchain.info/",
-    SERVER_URL: ""
+    SERVER_URL: "https://vanaheimex.com/testnet/"
   }
 }
