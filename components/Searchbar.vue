45706d64190144f7a191e6a97583b9134ad41399
HooriRn <hoorirn@protonmail.com>
2022-08-03 15:17:38 +0430

    Add other explorer network instances

--- components/Searchbar.vue
+++ components/Searchbar.vue
@@ -11,8 +11,30 @@
       >
       <SearchIcon class="search-icon" @click="find()"/>
     </div>
-    <SunIcon @click="changeTheme" v-if="theme === 'light'" class="social-icon"/>
-    <MoonIcon @click="changeTheme" v-if="theme === 'dark'" class="social-icon"/>
+    <div class="right-section">
+      <div id="network-wrapper">
+        <div class="network-container" @click="toggleDialog" ref="network">
+          <span>{{networkEnv | capitalize}}</span>
+        </div>
+        <div 
+          class="network-dialog" 
+          v-show="showDialog"
+          ref="netDialog"
+        >
+          <a :class="{'active': networkEnv == 'mainnet'}" :disabled="networkEnv == 'mainnet'" :href="gotoInstance('mainnet', networkEnv == 'mainnet')">
+            Mainnet
+          </a>
+          <a :class="{'active': networkEnv == 'stagenet'}" :disabled="networkEnv == 'stagenet'" :href="gotoInstance('stagenet', networkEnv == 'stagenet')">
+            Stagenet
+          </a>
+          <a :class="{'active': networkEnv == 'testnet'}" :disabled="networkEnv == 'testnet'"  :href="gotoInstance('testnet', networkEnv == 'testnet')">
+            Testnet
+          </a>
+        </div>
+      </div>
+      <SunIcon @click="changeTheme" v-if="theme === 'light'" class="social-icon"/>
+      <MoonIcon @click="changeTheme" v-if="theme === 'dark'" class="social-icon"/>
+    </div>
   </div>
 </template>
 
@@ -21,6 +43,7 @@ import SunIcon from '~/assets/images/eclipse-sun.svg?inline';
 import MoonIcon from '~/assets/images/eclipse-moon.svg?inline';
 import SearchIcon from '~/assets/images/search.svg?inline';
 import { mapGetters } from 'vuex';
+import links from '~/const/links';
 
 export default {
   name: 'SearchBar',
@@ -33,6 +56,7 @@ export default {
     return {
       searchQuery: '',
       isSearch: false,
+      showDialog: false,
     }
   },
   methods: {
@@ -78,6 +102,22 @@ export default {
     },
     search() {
       this.isSearch = true;
+    },
+    toggleDialog() {
+      this.showDialog = !this.showDialog;
+    },
+    gotoInstance(instance, disabled) {
+      if (disabled)
+        return
+      return links[instance];
+    },
+    dialogPos() {
+      if (this.$refs.network) {
+        let left = this.$refs.network.getBoundingClientRect().left;
+        let top = this.$refs.network.getBoundingClientRect().top;
+        this.$refs.netDialog.style.left = `${left}px`
+        this.$refs.netDialog.style.top = `${top+45}px`
+      }
     }
   },
   watch:{
@@ -88,7 +128,10 @@ export default {
   computed: {
     ...mapGetters({
       theme: 'getTheme'
-    })
+    }),
+    networkEnv: function() {
+      return process.env.NETWORK;
+    },
   },
   mounted() {
     window.addEventListener('click', (e) => {
@@ -96,7 +139,17 @@ export default {
         this.isSearch = false;
       }
     });
-  }
+
+    window.addEventListener('click', (e) => {
+      if (!document.getElementById('network-wrapper').contains(e.target)){
+        this.showDialog = false;
+      }
+    });
+
+    window.addEventListener('resize', this.dialogPos);
+
+    this.dialogPos();    
+  },
 }
 </script>
 
@@ -116,6 +169,73 @@ export default {
     fill: var(--font-color);
     width: 1rem;
     height: 1rem;
+  }
+
+  .right-section {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+
+    #network-wrapper {
+      .network-container {
+        padding: .5rem 1rem;
+        border-radius: .5rem;
+        background-color: var(--card-bg-color);
+        border: 1px solid var(--border-color);
+        width: 100px;
+        display: flex;
+        justify-content: center;
+        cursor: pointer;
+        
+        span {
+          text-align: center;
+          color: var(--primary-color);
+        }
+
+      }
+
+      .network-dialog {
+        position: absolute;
+        z-index: 1000;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid var(--border-color);
+        border-radius: .5rem;
+        width: 100px;
+
+        a {
+          cursor: pointer;
+          background: var(--card-bg-color);
+          color: var(--font-color);
+          border: none;
+          padding: .5rem 1rem;
+          text-decoration: none;
+          text-align: center;
+
+          &:first-of-type {
+            border-radius: .5rem .5rem 0 0;
+          }
+
+          &:last-of-type {
+            border-radius: 0 0 .5rem .5rem;
+          }
+
+          &:hover {
+            background: var(--darker-bg);
+            color: var(--primary-color);
+          }
+
+          &.active {
+            color: var(--primary-color);
+
+            &:hover {
+              background-color: var(--card-bg-color);
+            }
+          }
+        }
+      }
+    }
   }
 
   #search-container {
---
+++ const/links.js
@@ -0,0 +1,5 @@
+export default {
+  mainnet: 'https://thorchain.net',
+  stagenet: 'https://stagenet-thorchain-explorer-v2.vercel.app/',
+  testnet: 'https://testnet-thorchain-explorer-v2.vercel.app/'
+}
