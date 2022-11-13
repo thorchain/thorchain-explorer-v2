4a8726232ea4a7005abab64cadc675299b7209a2
HooriRn <hoorirn@protonmail.com>
2022-11-13 16:29:33 +0000

    Add savers vault info

--- pages/pools/index.vue
+++ pages/pools/index.vue
@@ -1,46 +1,78 @@
 <template>
   <Page>
-    <Nav :active-mode.sync="mode" :nav-items="navItems" />
-    <template v-if="mode == 'grid'">
-      <pool-card :pools="sortedPools" />
-    </template>
-    <template v-else-if="pools && pools.length > 0 && mode == 'table'">
-      <Nav :active-mode.sync="tableMode" :nav-items="tableModeItems" />
-      <template v-for="(k, v, i) in tables">
-        <vue-good-table
-          v-if="k.data.length > 0"
-          v-show="tableMode == k.mode"
-          :key="i"
-          :columns="cols"
-          :rows="k.data"
-          style-class="vgt-table net-table"
-          :pagination-options="{
-            enabled: true,
-            perPage: 30,
-            perPageDropdownEnabled: false,
-          }"
-          :sort-options="{
-            enabled: true,
-            initialSortBy: {field: 'apy', type: 'desc'}
-          }"
-          @on-row-click="gotoPoolTable"
-        >
-          <template slot="table-row" slot-scope="props">
-            <div v-if="props.column.field == 'asset'" v-tooltip="props.row.asset" class="cell-content">
-              <AssetIcon :asset="props.row.asset" />
-              <span>{{ props.formattedRow[props.column.field] }}</span>
-            </div>
-            <span v-else-if="props.column.field == 'status'">
-              <div :class="['bubble-container', {'yellow': k.mode == 'staged'}]">
-                <span>{{ props.row.status | capitalize }}</span>
-              </div>
-            </span>
-            <span v-else>
-              {{ props.formattedRow[props.column.field] }}
-            </span>
-          </template>
-        </vue-good-table>
+    <Nav :active-mode.sync="poolType" :nav-items="poolTypes" />
+    <!-- <Nav :active-mode.sync="viewMode" :nav-items="viewPools" /> -->
+    <template v-if="poolType == 'regular'">
+      <template v-if="viewMode == 'grid'">
+        <pool-card :pools="sortedPools" />
       </template>
+      <div v-else-if="pools && pools.length > 0 && viewMode == 'table'" class="pools-box">
+        <Nav :active-mode.sync="tableMode" :nav-items="tableModeItems" :extra-classes="['pools-type-table']"/>
+        <template v-for="(k, v, i) in tables">
+          <vue-good-table
+            v-if="k.data.length > 0"
+            v-show="tableMode == k.mode"
+            :key="i"
+            :columns="poolCols"
+            :rows="k.data"
+            style-class="vgt-table net-table"
+            :pagination-options="{
+              enabled: true,
+              perPage: 30,
+              perPageDropdownEnabled: false,
+            }"
+            :sort-options="{
+              enabled: true,
+              initialSortBy: {field: 'apy', type: 'desc'}
+            }"
+            @on-row-click="gotoPoolTable"
+          >
+            <template slot="table-row" slot-scope="props">
+              <div v-if="props.column.field == 'asset'" v-tooltip="props.row.asset" class="cell-content">
+                <AssetIcon :asset="props.row.asset" />
+                <span>{{ props.formattedRow[props.column.field] }}</span>
+              </div>
+              <span v-else-if="props.column.field == 'status'">
+                <div :class="['bubble-container', {'yellow': k.mode == 'staged'}]">
+                  <span>{{ props.row.status | capitalize }}</span>
+                </div>
+              </span>
+              <span v-else>
+                {{ props.formattedRow[props.column.field] }}
+              </span>
+            </template>
+          </vue-good-table>
+        </template>
+      </div>
+    </template>
+    <template v-else>
+      <vue-good-table
+        v-if="tables.activeRows.data.length > 0"
+        :columns="saverCols"
+        :rows="tables.saversRows.data"
+        style-class="vgt-table net-table"
+        :pagination-options="{
+          enabled: true,
+          perPage: 30,
+          perPageDropdownEnabled: false,
+        }"
+        @on-row-click="gotoPoolTable"
+      >
+        <template slot="table-row" slot-scope="props">
+          <div v-if="props.column.field == 'asset'" v-tooltip="props.row.asset" class="cell-content">
+            <AssetIcon :asset="props.row.asset" />
+            <span>{{ props.formattedRow[props.column.field] }}</span>
+          </div>
+          <span v-else-if="props.column.field == 'status'">
+            <div :class="['bubble-container', {'yellow': k.mode == 'staged'}]">
+              <span>{{ props.row.status | capitalize }}</span>
+            </div>
+          </span>
+          <span v-else>
+            {{ props.formattedRow[props.column.field] }}
+          </span>
+        </template>
+      </vue-good-table>
     </template>
   </Page>
 </template>
@@ -52,8 +84,13 @@ export default {
   data () {
     return {
       error: false,
-      mode: 'table',
-      navItems: [
+      poolType: 'regular',
+      poolTypes: [
+        { text: 'Pools', mode: 'regular' },
+        { text: 'Savers', mode: 'savers' }
+      ],
+      viewMode: 'table',
+      viewPools: [
         { text: 'Grid', mode: 'grid' },
         { text: 'Table', mode: 'table' }
       ],
@@ -62,7 +99,7 @@ export default {
         { text: 'Staged/Suspended Pools', mode: 'staged' }
       ],
       tableMode: 'active',
-      cols: [
+      poolCols: [
         {
           label: 'Asset',
           field: 'asset',
@@ -83,14 +120,14 @@ export default {
           label: 'Volume 24H',
           field: 'volume',
           type: 'number',
-          formatFn: this.numberFormat,
+          formatFn: this.formattedPrice,
           tdClass: 'mono'
         },
         {
           label: 'Depth',
           field: 'depth',
           type: 'number',
-          formatFn: this.numberFormat,
+          formatFn: this.formattedPrice,
           tdClass: 'mono'
         },
         {
@@ -106,6 +143,40 @@ export default {
           tdClass: 'mono'
         }
       ],
+      saverCols: [
+        {
+          label: 'Asset',
+          field: 'asset',
+          formatFn: this.formatAsset
+        },
+        {
+          label: 'USD Price',
+          field: 'price',
+          type: 'number',
+          formatFn: this.curFormat,
+          tdClass: 'mono'
+        },
+        {
+          label: 'Saver Depth',
+          field: 'saversDepth',
+          type: 'number',
+          formatFn: this.formattedPrice,
+          tdClass: 'mono'
+        },
+        {
+          label: 'Total Saver Units',
+          field: 'saversUnits',
+          type: 'number',
+          formatFn: this.numberFormat,
+          tdClass: 'mono'
+        },
+        {
+          label: 'Total Depth/Units Ratio',
+          field: 'depthToUnitsRatio',
+          type: 'number',
+          tdClass: 'mono'
+        }
+      ],
       pools: undefined,
       tables: {
         activeRows: {
@@ -115,6 +186,9 @@ export default {
         standbyRows: {
           data: [],
           mode: 'staged'
+        },
+        saversRows: {
+          data: []
         }
       }
     }
@@ -143,16 +217,23 @@ export default {
         apy: p.poolAPY,
         volume: (+p.volume24h / 10 ** 8) * runePrice,
         vd: (+p.volume24h) / ((+p.assetDepth * +p.assetPrice) + (+p.runeDepth)),
-        asset: p.asset
+        asset: p.asset,
+        saversDepth: (+p.saversDepth / 10 ** 8) * p.assetPriceUSD,
+        saversUnits: (+p.saversUnits),
+        depthToUnitsRatio: this.$options.filters.number(+p.saversDepth / +p.saversUnits, '0.00000')
       }))
       this.sepPools(ps)
+      this.setSavers(ps)
     }).catch((e) => {
       console.error(e)
     })
   },
   methods: {
-    numberFormat (number, filter) {
+    formattedPrice (number, filter) {
       return '$' + this.$options.filters.number(number, '0.00a')
+    },
+    numberFormat (number, filter) {
+      return this.$options.filters.number(number, '0.00a')
     },
     curFormat (number) {
       return this.$options.filters.currency(number)
@@ -176,10 +257,28 @@ export default {
           this.tables.standbyRows.data.push(pools[i])
         }
       }
+    },
+    setSavers (pools) {
+      if (!pools && pools.length <= 0) {
+        return
+      }
+      for (const i in pools) {
+        if (+pools[i].saversUnits > 0) {
+          this.tables.saversRows.data.push(pools[i])
+        }
+      }
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+.pools-box {
+  .nav-headers.box.pools-type-table {
+    margin: 0 !important;
+    border: 1px solid var(--border-color);
+    border-bottom: 0;
+    border-radius: 7px 8px 0 0;
+  }
+}
 </style>
