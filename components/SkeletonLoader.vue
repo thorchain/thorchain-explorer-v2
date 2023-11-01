cca4a97701897c9ba7ca58c3d79f3d614f1b35ba
HooriRn <hoorirn@protonmail.com>
2023-11-02 00:35:03 +0330

    pulse animation fix

--- components/SkeletonLoader.vue
+++ components/SkeletonLoader.vue
@@ -18,14 +18,14 @@ export default {
 
 <style lang="scss">
 .skeleton-container {
-  animation: pulse 2s cubic-bezier(.4, 0, .6, 1) infinite;
+  animation: skeleton 2s cubic-bezier(.4, 0, .6, 1) infinite;
   background-color: var(--active-bg-color);
   height: .5rem;
   border-radius: 1rem;
   margin-top: .75rem;
 }
 
-@keyframes pulse {
+@keyframes skeleton {
   50% {
     opacity: .5;
   }
