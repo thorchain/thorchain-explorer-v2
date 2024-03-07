a7b0e91b3259aec26d33addc12a90aea0b896f9b
HooriRn <hoorirn@protonmail.com>
2024-03-07 14:11:10 +0330

    Update insight urls

--- api/insights.api.js
+++ api/insights.api.js
@@ -1,7 +1,7 @@
 
 import { $axiosInstace } from './index'
 
-const INSIGHT_URL = 'https://node-api.flipsidecrypto.com/api/v2/queries/'
+const INSIGHT_URL = 'https://flipsidecrypto.xyz/api/v1/queries/'
 
 export function getChurnHistory () {
   return $axiosInstace.get(INSIGHT_URL + '9fb2fd10-0a5a-4786-af5e-d5b346bcc220/data/latest')
