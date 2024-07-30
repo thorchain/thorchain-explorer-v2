187ede3d3f281cf7f27d5c799ac963485459410a
yasi-amani <yasamanamanii7020@gmail.com>
2024-07-30 11:29:02 +0430

    add:Overview-synths

--- pages/thorfi/synths.vue
+++ pages/thorfi/synths.vue
@@ -1,5 +1,6 @@
 <template>
   <Page>
+    <cards-header :tableGeneralStats="synthsGeneralStats" />
     <Card title="Synth Assets" :isLoading="!(rows && rows.length > 0)">
       <vue-good-table
         v-if="cols && rows.length > 0"
@@ -13,15 +14,27 @@
         }"
         :sort-options="{
           enabled: true,
-          initialSortBy: {field: 'utilisation', type: 'desc'}
+          initialSortBy: { field: 'utilisation', type: 'desc' },
         }"
       >
         <template slot="table-column" slot-scope="props">
-          <div v-if="props.column.field == 'saverPercentage'" v-tooltip="'Savers depth to the Synth Supply'" class="table-asset">
+          <div
+            v-if="props.column.field === 'saverPercentage'"
+            v-tooltip="'Savers depth to the Synth Supply'"
+            class="table-asset"
+          >
             Saver %
             <info-icon class="header-icon" />
           </div>
-          <div v-else-if="props.column.field == 'utilisation'" v-tooltip="`Synth supply to the Pool Depth relative to the Synth cap (${(synthCap*100).toFixed(2)}%)`" class="table-asset">
+          <div
+            v-else-if="props.column.field === 'utilisation'"
+            v-tooltip="
+              `Synth supply to the Pool Depth relative to the Synth cap (${(
+                synthCap * 100
+              ).toFixed(2)}%)`
+            "
+            class="table-asset"
+          >
             Utilisation
             <info-icon class="header-icon" />
           </div>
@@ -30,36 +43,43 @@
           </span>
         </template>
         <template slot="table-row" slot-scope="props">
-          <div v-if="props.column.field == 'asset'" v-tooltip="props.row.asset" class="cell-content">
+          <div
+            v-if="props.column.field === 'asset'"
+            v-tooltip="props.row.asset"
+            class="cell-content"
+          >
             <AssetIcon :asset="props.row.asset" chain="THOR.RUNE" />
             <span>{{ props.formattedRow[props.column.field] }}</span>
           </div>
-          <span v-else-if="props.column.field == 'synth'">
-            <span v-tooltip="props.row.synth">{{ props.formattedRow[props.column.field] }}</span>
+          <span v-else-if="props.column.field === 'synth'">
+            <span v-tooltip="props.row.synth">{{
+              props.formattedRow[props.column.field]
+            }}</span>
           </span>
-          <span v-else-if="props.column.field == 'supply'">
-            <span>{{ props.formattedRow[props.column.field] }}
+          <span v-else-if="props.column.field === 'supply'">
+            <span
+              >{{ props.formattedRow[props.column.field] }}
               <span class="extra-text">
                 {{ showAsset(props.row.asset) }}
               </span>
             </span>
           </span>
-          <span v-else-if="props.column.field == 'saverUtilisation'">
-            <span v-if="props.row.saverUtilisation != 0">
+          <span v-else-if="props.column.field === 'saverUtilisation'">
+            <span v-if="props.row.saverUtilisation !== 0">
               {{ props.formattedRow[props.column.field] }}
             </span>
-            <span v-else>
-              -
-            </span>
+            <span v-else> - </span>
           </span>
-          <span v-else-if="props.column.field == 'utilisation'">
+          <span v-else-if="props.column.field === 'utilisation'">
             <span v-if="props.row.utilisation < 1">
               {{ props.formattedRow[props.column.field] }}
-              <span v-if="props.row.isPol == true" style="color: var(--primary-color)">(POL Cap)</span>
+              <span
+                v-if="props.row.isPol === true"
+                style="color: var(--primary-color)"
+                >(POL Cap)</span
+              >
             </span>
-            <span v-else style="color: var(--primary-color)">
-              Filled
-            </span>
+            <span v-else style="color: var(--primary-color)"> Filled </span>
           </span>
           <span v-else>
             {{ props.formattedRow[props.column.field] }}
@@ -71,25 +91,28 @@
 </template>
 
 <script>
-import { formatAsset, synthToAsset } from '~/utils'
-import InfoIcon from '~/assets/images/info.svg?inline'
+import { mapGetters } from "vuex";
+import { formatAsset, synthToAsset } from "~/utils";
+import InfoIcon from "~/assets/images/info.svg?inline";
 
 export default {
   components: { InfoIcon },
-  async asyncData ({ $api }) {
-    const synthAssets = (await $api.getAssets().catch(e => console.error(e))).data
-    const pools = (await $api.getThorPools().catch(e => console.error(e))).data
-    const { data: mimirData } = await $api.getMimir()
-    const polCap = (mimirData.POLTARGETSYNTHPERPOOLDEPTH + mimirData.POLBUFFER) / 10000
-    const synthCap = mimirData.MAXSYNTHPERPOOLDEPTH / 10000
-    const synthUtils = []
+  async asyncData({ $api }) {
+    const synthAssets = (await $api.getAssets().catch((e) => console.error(e)))
+      .data;
+    const pools = (await $api.getThorPools().catch((e) => console.error(e)))
+      .data;
+    const { data: mimirData } = await $api.getMimir();
+    const polCap =
+      (mimirData.POLTARGETSYNTHPERPOOLDEPTH + mimirData.POLBUFFER) / 10000;
+    const synthCap = mimirData.MAXSYNTHPERPOOLDEPTH / 10000;
+    const synthUtils = [];
     for (const asset of synthAssets.supply) {
-      // bnb chain is deprecated
-      if (asset.denom === 'bnb/bnb') {
-        continue
+      if (asset.denom === "bnb/bnb") {
+        continue;
       }
-      const assetName = synthToAsset(asset.denom)
-      const pool = pools.find(p => p.asset === assetName)
+      const assetName = synthToAsset(asset.denom);
+      const pool = pools.find((p) => p.asset === assetName);
       synthUtils.push({
         asset: assetName,
         synth: asset.denom,
@@ -97,71 +120,117 @@ export default {
         synth_supply: pool?.synth_supply,
         asset_depth: pool?.balance_asset,
         savers_depth: pool?.savers_depth,
-        units: pool?.pool_units
-      })
+        units: pool?.pool_units,
+      });
     }
-    return { pools, synthAssets, synthUtils, mimirData, polCap, synthCap }
+    return { pools, synthAssets, synthUtils, mimirData, polCap, synthCap };
   },
-  data () {
+  data() {
     return {
+      synthsGeneralStats: {},
       cols: [
         {
-          label: 'Asset',
-          field: 'asset',
-          formatFn: formatAsset
+          label: "Asset",
+          field: "asset",
+          formatFn: formatAsset,
         },
         {
-          label: 'Synth',
-          field: 'synth',
-          formatFn: formatAsset
+          label: "Synth",
+          field: "synth",
+          formatFn: formatAsset,
         },
         {
-          label: 'Saver %',
-          field: 'saverPercentage',
-          type: 'percentage',
-          tdClass: 'mono',
-          thClass: 'end'
+          label: "Saver %",
+          field: "saverPercentage",
+          type: "percentage",
+          tdClass: "mono",
+          thClass: "end",
         },
         {
-          label: 'Utilisation',
-          field: 'utilisation',
-          type: 'percentage',
-          tdClass: 'mono',
-          thClass: 'end'
+          label: "Utilisation",
+          field: "utilisation",
+          type: "percentage",
+          tdClass: "mono",
+          thClass: "end",
         },
         {
-          label: 'Supply',
-          field: 'supply',
-          type: 'number',
-          tdClass: 'mono',
-          formatFn: this.numberFormat
-        }
+          label: "Supply",
+          field: "supply",
+          type: "number",
+          tdClass: "mono",
+          formatFn: this.numberFormat,
+        },
       ],
-      rows: []
+      rows: [],
+    };
+  },
+  computed: {
+    totalSynthSupplyUSD() {
+      return this.synthUtils.reduce((total, o) => {
+        if (o.synth_supply > 0) {
+          return total + this.amountToUSD(o.asset, o.synth_supply, this.Pools);
+        } else {
+          return total;
+        }
+      }, 0);
+    },
+    totalUtilisationUSD() {
+      return this.synthUtils.reduce((total, o) => {
+        if (o.savers_depth > 0) {
+          return total + this.amountToUSD(o.asset, o.savers_depth, this.Pools);
+        } else {
+          return total;
+        }
+      }, 0);
+    },
+    totalSaverPercentage() {
+      return this.totalSynthSupplyUSD > 0
+        ? this.totalUtilisationUSD / this.totalSynthSupplyUSD
+        : 0;
+    },
+    ...mapGetters({
+      Pools: "getPools",
+    }),
+  },
+  async mounted() {
+    if (this.synthUtils && this.synthUtils.length > 0) {
+      this.rows = this.synthUtils.map(asset => ({
+        asset: asset?.asset,
+        synth: asset?.synth,
+        utilisation:
+          (+asset?.synth_supply / (+asset?.asset_depth * 2)) *
+          (1 / this.synthCap),
+        isPol:
+          +asset?.synth_supply / (+asset?.asset_depth * 2) >= this.polCap,
+        saverPercentage: +asset?.savers_depth / +asset?.synth_supply,
+        supply: +asset?.synth_supply / 10 ** 8,
+      }));
+      this.updateGeneralStats();
     }
   },
-  mounted () {
-    if (this.synthUtils && this.synthUtils.length > 0) {
-      for (const asset of this.synthUtils) {
-        if (!asset) {
-          continue
-        }
-        this.rows.push({
-          asset: asset?.asset,
-          synth: asset?.synth,
-          utilisation: (+asset?.synth_supply / (+asset?.asset_depth * 2)) * (1 / this.synthCap),
-          isPol: (+asset?.synth_supply / (+asset?.asset_depth * 2)) >= this.polCap,
-          saverPercentage: +asset?.savers_depth / +asset?.synth_supply,
-          supply: +asset?.synth_supply / 10 ** 8
-        })
-      }
-    }
-  }
-}
+  methods: {
+    updateGeneralStats() {
+      this.synthsGeneralStats = [
+        {
+          name: "Total Synth Supply:",
+          value: this.$options.filters.currency(this.totalSynthSupplyUSD),
+        },
+        {
+          name: "Total Utilisation:",
+          value: this.$options.filters.currency(this.totalUtilisationUSD),
+        },
+        {
+          name: "Total Saver Percentage:",
+          value: this.$options.filters.percent(this.totalSaverPercentage),
+        },
+        { name: "POL Cap:", value: this.$options.filters.percent(this.polCap) },
+      ];
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
 th.end .table-asset {
   justify-content: flex-end;
 }
