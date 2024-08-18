1a271fd724d562f6b5c5b56ff69063bef50e14c9
HooriRn <hoorirn@protonmail.com>
2024-08-18 22:21:34 +0330

    Add fees/reward charts

--- pages/insights/index.vue
+++ pages/insights/index.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="container-page">
-    <div class="swaps-insight">
+    <div class="two-insight">
       <Card title="Swap Chart (from Flipside)">
         <VChart
-          v-if="swapChartVolume"
           :option="swapChartVolume"
           :loading="!swapChartVolume"
           :autoresize="true"
@@ -11,9 +10,24 @@
       </Card>
       <Card title="Swap Chart Normalized (from Flipside)">
         <VChart
-          v-if="swapChartVolumeNorm"
           :option="swapChartVolumeNorm"
           :loading="!swapChartVolumeNorm"
+          :autoresize="true"
+        />
+      </Card>
+    </div>
+    <div class="two-insight">
+      <Card title="Fees/Reward Chart (from Flipside)">
+        <VChart
+          :option="feesRewardsChart"
+          :loading="!feesRewardsChart"
+          :autoresize="true"
+        />
+      </Card>
+      <Card title="Fees/Reward Chart Normalized (from Flipside)">
+        <VChart
+          :option="feesRewardsChartNorm"
+          :loading="!feesRewardsChartNorm"
           :autoresize="true"
         />
       </Card>
@@ -74,19 +88,232 @@ export default {
       tvlOption: undefined,
       swapChartVolumeNorm: undefined,
       swapChartVolume: undefined,
+      feesRewardsChart: undefined,
+      feesRewardsChartNorm: undefined,
     }
   },
   mounted() {
     this.$api.getSwapsWeekly().then(({ data }) => {
-      this.dailySwapFormat(data)
+      this.swapsStats(data)
     })
 
     this.$api.getStatsDaily().then(({ data }) => {})
 
-    this.$api.getFeesRewardsMonthly().then(({ data }) => {})
+    this.$api.getFeesRewardsMonthly().then(({ data }) => {
+      this.feesRewards(data)
+    })
   },
   methods: {
-    dailySwapFormat(d) {
+    focusFormatter(series, totalVolume, axis = false) {
+      let colors = ['#63FDD9', '#00CCFF', '#F3BA2F', '#FF4954']
+      if (this.theme === 'light') {
+        colors = ['#3ca38b', '#00CCFF', '#F3BA2F', '#FF4954']
+      }
+
+      if (axis) {
+        const formatter = (param) => {
+          return `
+          <div class="tooltip-header">
+            ${param[0].name}
+          </div>
+          <div class="tooltip-body">
+            ${param
+              .map((p) => {
+                if (p.value > 0) {
+                  return `
+                <div class="tooltip-body">
+                  <span>
+                    <span class="series-name-color">
+                      <div class="data-color" style="background-color: ${p.color}"></div>
+                      ${p.seriesName}
+                    </span>
+                    <b>$${this.$options.filters.number(p.value, '0,0.00 a')}</b>
+                  </span>
+                </div>
+                `
+                } else {
+                  return ''
+                }
+              })
+              .join('\n')}
+            <span class="tooltip-total">
+              <span>Total</span>
+              <b>$${this.$options.filters.number(totalVolume[param[0].dataIndex], '0,0.00 a')}</b>
+            </span>
+          </div>
+        `
+        }
+
+        return formatter
+      }
+
+      const formatter = (param) => {
+        return `
+          <div class="tooltip-header">
+            ${param.name}
+          </div>
+          <div class="tooltip-body">
+            ${series
+              .map((p, i) => {
+                if (p.data[param.dataIndex] > 0) {
+                  return `
+                  <span>
+                    <span class="series-name-color">
+                      <div class="data-color" style="background-color: ${colors[i]}"></div>
+                      ${p.name}
+                    </span>
+                    <b style="color: ${param.seriesIndex === i ? param.color : 'rgb(102, 102, 102)'}">$${this.$options.filters.number(p.data[param.dataIndex], '0,0.00 a')}</b>
+                  </span>
+                `
+                } else {
+                  return ''
+                }
+              })
+              .join('\n')}
+            <span class="tooltip-total">
+              <span>Total</span>
+              <b>$${this.$options.filters.number(totalVolume[param.dataIndex], '0,0.00 a')}</b>
+            </span>
+          </div>
+        `
+      }
+
+      return formatter
+    },
+    feesRewards(d) {
+      const xAxis = []
+      const liquidityFees = []
+      const rewards = []
+      const halfLine = []
+      const pctLiquidityFees = []
+      const totalVolume = []
+      d.forEach((interval, index) => {
+        xAxis.push(moment(interval.date).format('YY/MM/DD'))
+        liquidityFees.push(interval.liquidity_fees_usd)
+        rewards.push(interval.block_rewards_usd)
+        halfLine.push(interval.half_line)
+        totalVolume.push(interval.fees_plus_rewards)
+        pctLiquidityFees.push(interval.pct_liquidty_fees)
+      })
+
+      const series = [
+        {
+          name: 'Liquidity Fees',
+          type: 'bar',
+          stack: 'Total',
+          showSymbol: false,
+          symbol: 'circle',
+          emphasis: {
+            focus: 'series',
+          },
+          data: liquidityFees,
+        },
+        {
+          name: 'Trade Swap Volume',
+          type: 'bar',
+          stack: 'Total',
+          showSymbol: false,
+          symbol: 'circle',
+          emphasis: {
+            focus: 'series',
+          },
+          data: rewards,
+        },
+      ]
+
+      const formatter = this.focusFormatter(series, totalVolume, true)
+
+      this.feesRewardsChart = this.basicChartFormat(
+        undefined,
+        series,
+        xAxis,
+        {
+          legend: {
+            type: 'scroll',
+            pageIconColor: 'var(--primary-color)',
+            icon: 'rect',
+            textStyle: {
+              color: 'var(--sec-font-color)',
+            },
+          },
+          yAxis: [
+            {
+              type: 'value',
+              name: '',
+              position: 'right',
+              show: false,
+              splitLine: {
+                show: true,
+              },
+              max: 'dataMax',
+            },
+          ],
+        },
+        formatter
+      )
+
+      const normSeries = series.map((s, i) => {
+        return {
+          name: s.name,
+          type: 'bar',
+          stack: 'total',
+          yAxisIndex: 0,
+          data: s.data.map((d, j) => d / totalVolume[j]),
+        }
+      })
+
+      normSeries.push({
+        name: 'Half Line',
+        type: 'line',
+        stack: 'Total',
+        showSymbol: false,
+        symbol: 'circle',
+        emphasis: {
+          focus: 'series',
+        },
+        yAxisIndex: 1,
+        data: halfLine,
+      })
+
+      this.feesRewardsChartNorm = this.basicChartFormat(
+        (value) => `${this.percentageFormat(value, 2)}`,
+        normSeries,
+        xAxis,
+        {
+          legend: {
+            type: 'scroll',
+            pageIconColor: 'var(--primary-color)',
+            icon: 'rect',
+            textStyle: {
+              color: 'var(--sec-font-color)',
+            },
+          },
+          yAxis: [
+            {
+              type: 'value',
+              name: '',
+              position: 'right',
+              show: false,
+              splitLine: {
+                show: true,
+              },
+              max: 1,
+            },
+            {
+              type: 'value',
+              name: '',
+              position: 'left',
+              show: false,
+              splitLine: {
+                show: true,
+              },
+              max: 1,
+            },
+          ],
+        }
+      )
+    },
+    swapsStats(d) {
       const xAxis = []
       const taSwapVolume = []
       const synthSwapVolume = []
@@ -171,42 +398,7 @@ export default {
         },
       ]
 
-      let colors = ['#63FDD9', '#00CCFF', '#F3BA2F', '#FF4954']
-      if (this.theme === 'light') {
-        colors = ['#3ca38b', '#00CCFF', '#F3BA2F', '#FF4954']
-      }
-
-      const formatter = (param) => {
-        console.log(param)
-        return `
-          <div class="tooltip-header">
-            ${param.name}
-          </div>
-          <div class="tooltip-body">
-            ${series
-              .map((p, i) => {
-                if (p.data[param.dataIndex] > 0) {
-                  return `
-                  <span style="color: ${param.seriesIndex === i ? param.color : 'rgb(102, 102, 102)'}">
-                    <span class="series-name-color">
-                      <div class="data-color" style="background-color: ${colors[i]}"></div>
-                      ${p.name}
-                    </span>
-                    <b>$${this.$options.filters.number(p.data[param.dataIndex], '0,0.00 a')}</b>
-                  </span>
-                `
-                } else {
-                  return ''
-                }
-              })
-              .join('\n')}
-            <span class="tooltip-total">
-              <span>Total</span>
-              <b>$${this.$options.filters.number(totalVolume[param.dataIndex], '0,0.00 a')}</b>
-            </span>
-          </div>
-        `
-      }
+      const formatter = this.focusFormatter(series, totalVolume)
 
       this.swapChartVolume = this.basicChartFormat(
         undefined,
@@ -227,6 +419,18 @@ export default {
             formatter,
             className: 'custom-tooltip',
           },
+          yAxis: [
+            {
+              type: 'value',
+              name: '',
+              position: 'right',
+              show: false,
+              splitLine: {
+                show: true,
+              },
+              max: 'dataMax',
+            },
+          ],
         },
         undefined
       )
@@ -253,6 +457,18 @@ export default {
               color: 'var(--sec-font-color)',
             },
           },
+          yAxis: [
+            {
+              type: 'value',
+              name: '',
+              position: 'right',
+              show: false,
+              splitLine: {
+                show: true,
+              },
+              max: 1,
+            },
+          ],
         }
       )
     },
@@ -291,7 +507,7 @@ export default {
   gap: 10px;
 }
 
-.swaps-insight {
+.two-insight {
   display: flex;
   flex-wrap: wrap;
   gap: 8px;
