5a5cdc39952b63b27d2cd526fd22b8d7647bc278
HooriRn <hoorirn@protonmail.com>
2024-08-28 12:12:00 +0330

    Componentize utility box

---
+++ components/UtilityBox.vue
@@ -0,0 +1,33 @@
+<template lang="">
+  <div class="utility-wrapper" style="margin-left: 0.7rem">
+    <copy :str-copy="value" />
+    <div class="divider" />
+    <qr-btn :qrcode="value" />
+  </div>
+</template>
+<script>
+export default {
+  props: ['value'],
+}
+</script>
+<style lang="scss">
+.utility-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.4rem;
+  border-radius: 0.3rem;
+  background-color: var(--bg-color);
+
+  .divider {
+    margin: 0 8px;
+
+    &::after {
+      display: block;
+      content: ' ';
+      border-right: 1px solid var(--border-color);
+      height: 16px;
+    }
+  }
+}
+</style>
