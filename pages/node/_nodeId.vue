d7b2413926b26fe71ea7e4fb1159027539155893
yasi-amani <yasamanamanii7020@gmail.com>
2024-10-07 19:55:16 +0330

    refactor:node page

--- pages/node/_nodeId.vue
+++ pages/node/_nodeId.vue
@@ -1,14 +1,15 @@
 <template>
   <Page>
-    <div v-if="nodeSettings && nodeSettings.length > 0" class="node-container">
-      <stat-table :header="`Node: ${nodeId}`" :table-settings="nodeSettings">
+      <div class="node-header"> Node:<span class="node-id">{{ nodeId }}</span></div>
+      <info-card :options="nodeSettings">
         <template #address>
           <span class="clickable">
-            {{ address }}
+            {{address}}
           </span>
         </template>
-      </stat-table>
-      <card title="Providers" style="margin-top: 1rem" :loading="!node">
+      </info-card>
+      <div class="cards-node">
+      <card title="Providers" style="margin-top: 1rem" :is-loading="loading">
         <div v-if="node">
           <div class="providers-container">
             <div
@@ -36,7 +37,7 @@
           </div>
         </div>
       </card>
-      <card title="Signer Membership" style="margin-top: 1rem" :loading="!node">
+      <card title="Signer Membership" style="margin-top: 1rem" :is-loading="loading">
         <div v-if="node">
           <div class="providers-container">
             <div
@@ -54,24 +55,20 @@
         </div>
       </card>
     </div>
-    <div v-else class="loading">
-      <BounceLoader color="var(--font-color)" size="3rem" />
     </div>
   </Page>
 </template>
 
 <script>
-import BounceLoader from 'vue-spinner/src/BounceLoader.vue'
-import StatTable from '~/components/StatTable.vue'
 
 export default {
-  components: { StatTable, BounceLoader },
   async asyncData({ params }) {
     return { nodeId: params.nodeId }
   },
   data() {
     return {
       node: undefined,
+      loading:true,
     }
   },
   head: {
@@ -80,137 +77,168 @@ export default {
   computed: {
     nodeSettings() {
       return [
-        [
-          {
-            name: 'IP Address',
-            value: this.node?.ip_address,
-            filter: true,
+        {
+          title: 'Overview',
+          rowStart: 1,
+          colSpan: 1,
+          items: [
+            {
+              name: 'IP Address',
+              value: this.node?.ip_address,
+            },
+            {
+              name: 'Version',
+              value: this.node?.version,
+            },
+            {
+              name: 'Status',
+              value: this.node?.status,
+            },
+
+            {
+              name: 'Total Bond',
+              value: this.node?.total_bond / 10 ** 8,
+              filter: (v) =>
+                `${this.runeCur()} ${this.$options.filters.number(v, '0,0.00')}`,
+                usdValue: true,
+            },
+            {
+              name: 'Slash Points',
+              value: this.node?.slash_points,
+            },
+            {
+              name: 'Current Reward',
+              value: this.node?.current_award / 10 ** 8,
+              filter: (v) =>
+                `${this.runeCur()} ${this.$options.filters.number(v, '0,0.00')}`,
+                usdValue: true,
+            },
+
+            {
+              name: 'Active Since (Block Height)',
+              value:this.node?.active_block_height,
+              filter: (v) => `${this.$options.filters.number(v, '0,0')}`,
+            },
+            {
+              name: 'Status Since (Block Height)',
+              value:this.node?.status_since,
+              filter: (v) => `${this.$options.filters.number(v, '0,0')}`,
+            },
+
+            {
+              name: 'Node operator address',
+              value: this.node?.node_operator_address,
+              filter: (v) => this.addressFormatV2(v),
+
+            },
+            {
+              name: 'Public Keys: Secp256k1',
+              value: this.node?.pub_key_set?.secp256k1,
+              filter: (v) => this.addressFormatV2(v),
+            },
+
+            {
+              name: 'Public Keys: Ed25519',
+              value: this.node?.pub_key_set?.ed25519,
+              filter: (v) => this.addressFormatV2(v),
+            },
+          ]
           },
           {
-            name: 'Version',
-            value: this.node?.version,
-            filter: true,
+          title: 'Leave',
+          rowStart: 1,
+          colSpan: 1,
+          items: [
+            {
+              name: 'Requested To Leave',
+              value: this.node?.requested_to_leave?.toString(),
+            },
+            {
+              name: 'Forced To Leave',
+              value: this.node?.forced_to_leave?.toString(),
+            },
+            {
+              name: 'Leave Height',
+              value: this.node?.leave_height?.toString(),
+            },
+             {
+              header:'Jail'
+            },
+            {
+              name: 'Jail Release Height',
+              value: this.node?.jail?.release_height,
+              filter: (v) => `${this.$options.filters.number(v, '0,0')}`,
+            },
+            {
+              name: 'Jail Reason',
+              value: this.node?.jail?.reason,
+            },
+            {
+              header:'Preflight'
+            },
+            {
+              name: 'Preflight Status',
+              value: this.node?.preflight_status?.status,
+            },
+            {
+              name: 'Preflight Reason',
+              value: this.node?.preflight_status?.reason,
+            },
+            {
+              name: 'Preflight Code',
+              value: this.node?.preflight_status?.code,
+            },
+            ]
           },
-          {
-            name: 'Status',
-            value: this.node?.status,
-            filter: true,
-          },
-        ],
-        [
-          {
-            name: 'Total Bond',
-            value: Number.parseInt(this.node?.total_bond) / 10 ** 8,
-            filter: true,
-            runeValue: true,
-            usdValue: true,
-          },
-          {
-            name: 'Slash Points',
-            value: Number.parseInt(this.node?.slash_points),
-          },
-          {
-            name: 'Current Reward',
-            value: Number.parseInt(this.node?.current_award) / 10 ** 8,
-            filter: true,
-            runeValue: true,
-            usdValue: true,
-          },
-        ],
-        [
-          {
-            name: 'Active Since (Block Height)',
-            value: Number.parseInt(this.node?.active_block_height),
-          },
-          {
-            name: 'Status Since (Block Height)',
-            value: Number.parseInt(this.node?.status_since),
-          },
-        ],
-        [
-          {
-            name: 'Node operator address',
-            value: this.node?.node_operator_address,
-            filter: true,
-          },
-        ],
-        [
-          {
-            name: 'Public Keys: Secp256k1',
-            value: this.node?.pub_key_set?.secp256k1,
-            filter: true,
-          },
-        ],
-        [
-          {
-            name: 'Public Keys: Ed25519',
-            value: this.node?.pub_key_set?.ed25519,
-            filter: true,
-          },
-        ],
-        [
-          {
-            name: 'Requested To Leave',
-            value: this.node?.requested_to_leave?.toString(),
-            filter: true,
-          },
-          {
-            name: 'Forced To Leave',
-            value: this.node?.forced_to_leave?.toString(),
-            filter: true,
-          },
-          {
-            name: 'Leave Height',
-            value: this.node?.leave_height?.toString(),
-            filter: true,
-          },
-        ],
-        [
-          {
-            name: 'Jail Node Address',
-            value: this.node?.jail?.node_address,
-            filter: true,
-          },
-          {
-            name: 'Jail Release Height',
-            value: this.node?.jail?.release_height,
-            filter: true,
-          },
-          {
-            name: 'Jail Reason',
-            value: this.node?.jail?.reason,
-            filter: true,
-          },
-        ],
-        [
-          {
-            name: 'Preflight Status',
-            value: this.node?.preflight_status?.status,
-            filter: true,
-          },
-          {
-            name: 'Preflight Reason',
-            value: this.node?.preflight_status?.reason,
-            filter: true,
-          },
-          {
-            name: 'Preflight Code',
-            value: this.node?.preflight_status?.code,
-            filter: true,
-          },
-        ],
-      ]
+          ]
+        }
     },
-  },
-  mounted() {
-    this.$api.getNode(this.nodeId).then(({ data }) => {
-      this.node = data
+    mounted() {
+  this.loading = true;
+  this.$api.getNode(this.nodeId)
+    .then(({ data }) => {
+      this.node = data;
     })
-  },
+    .catch(error => {
+      console.error("Error fetching node data:", error);
+    })
+    .finally(() => {
+      this.loading = false; 
+    });
+}
+
 }
 </script>
 
 <style lang="scss" scoped>
+.node-header{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: var(--card-bg-color);
+  color: var(--sec-font-color);
+  border-radius: 0.5rem;
+  padding: 1rem;
+  margin-bottom: 1rem;
+}
+.node-id {
+  color: var(--primary-color);
+  padding-left: 0.5rem;
+
+}
+.cards-node {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    gap:10px
+}
+@include md{
+  .cards-node{
+    flex-direction: row;
+
+  }
+}
+
 .providers-container {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
