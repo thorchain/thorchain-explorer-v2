925fb01b10360a96cd65000b9bc0086443815701
HooriRn <hoorirn@protonmail.com>
2024-10-18 00:54:29 +0330

    Add block page

--- mixins.js/global.js
+++ mixins.js/global.js
@@ -797,5 +797,12 @@ export default {
         this.addAnimate(d, className, duration)
       }
     },
+    createDurationText(duration) {
+      const hours = String(duration.hours()).padStart(2, '0')
+      const minutes = String(duration.minutes()).padStart(2, '0')
+      const seconds = String(duration.seconds()).padStart(2, '0')
+
+      return `${hours}:${minutes}:${seconds}`
+    },
   },
 }
