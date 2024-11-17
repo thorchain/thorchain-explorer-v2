56e45e76e576d5e331a0d154f6f904146b7231e1
HooriRn <77393482+HooriRn@users.noreply.github.com>
2024-11-18 00:45:43 +0330

    Add overall info to the leaderboard (#329)
    * Add overall info to the leaderboard
    
    * Update
    
    ---------
    
    Co-authored-by: yasi-amani <yasamanamanii7020@gmail.com>

--- pages/insights/leaderboard.vue
+++ pages/insights/leaderboard.vue
@@ -1,10 +1,16 @@
 <template>
   <page>
     <Nav :active-mode.sync="period" :nav-items="periods" pre-text="Period :" />
+    <cards-header
+      :table-general-stats="
+        period === 'month' ? overallInfoMonthly : overallInfoWeekly
+      "
+    />
     <affiliate-table
       v-if="period === 'month'"
       :affiliate-data="affiliateDataMonthly"
     ></affiliate-table>
+
     <affiliate-table
       v-if="period === 'week'"
       :affiliate-data="affiliateDataWeekly"
@@ -23,6 +29,8 @@ export default {
     return {
       affiliateDataMonthly: [],
       affiliateDataWeekly: [],
+      overallInfoMonthly: null,
+      overallInfoWeekly: null,
       period: 'month',
       periods: [
         { text: '1 Week', mode: 'week' },
@@ -31,23 +39,26 @@ export default {
     }
   },
   mounted() {
-    try {
-      this.$api.getAffiliateSwapsByWallet().then(({ data }) => {
-        this.affiliateDataMonthly = this.formatData(data)
-      })
-    } catch (error) {
-      console.error('Error fetching affiliate data:', error)
-    }
-
-    try {
-      this.$api.getAffiliateSwapsWeekly().then(({ data }) => {
-        this.affiliateDataWeekly = this.formatData(data)
-      })
-    } catch (error) {
-      console.error('Error fetching affiliate data:', error)
-    }
+    this.fetchAffiliateData()
   },
   methods: {
+    async fetchAffiliateData() {
+      try {
+        const { data } = await this.$api.getAffiliateSwapsByWallet()
+        this.affiliateDataMonthly = this.formatData(data)
+        this.overallInfoMonthly = this.calculateOverallInfo(data)
+      } catch (error) {
+        console.error('Error fetching monthly affiliate data:', error)
+      }
+
+      try {
+        const { data } = await this.$api.getAffiliateSwapsWeekly()
+        this.affiliateDataWeekly = this.formatData(data)
+        this.overallInfoWeekly = this.calculateOverallInfo(data)
+      } catch (error) {
+        console.error('Error fetching weekly affiliate data:', error)
+      }
+    },
     mapMissing(item) {
       switch (item.affiliate) {
         case 'Edge Wallet':
@@ -61,6 +72,43 @@ export default {
         default:
           return item.affiliate
       }
+    },
+    calculateOverallInfo(data) {
+      const totalAffiliates = data.reduce(
+        (sum, item) => sum + item.affiliate_fees_usd,
+        0
+      )
+      const totalSwaps = data.reduce((sum, item) => sum + item.total_swaps, 0)
+      const totalVolume = data.reduce(
+        (sum, item) => sum + +item.total_volume_usd,
+        0
+      )
+
+      const avgFee = data.reduce((sum, item) => {
+        const volumeRatio = item.total_volume_usd / totalVolume
+        return sum + item.avg_affiliate_fee_basis_points * volumeRatio
+      }, 0)
+
+      const volumePerSwap = totalVolume / totalSwaps
+
+      return [
+        {
+          name: 'Affiliates Collected',
+          value: `$${this.$options.filters.number(totalAffiliates, '0.00a')}`,
+        },
+        {
+          name: 'AVG Fee',
+          value: `${this.$options.filters.percent(avgFee / 1e4, 2)}`,
+        },
+        {
+          name: 'Swap Count',
+          value: this.$options.filters.number(totalSwaps, '0a'),
+        },
+        {
+          name: 'Volume / Count',
+          value: `$${this.$options.filters.number(volumePerSwap, '0.00a')}`,
+        },
+      ]
     },
     formatData(data) {
       return data.map((item) => {
@@ -78,4 +126,30 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.leaderboard-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 20px;
+  flex-wrap: wrap;
+
+  @include md {
+    flex-direction: row;
+  }
+
+  .data-section {
+    flex: 1;
+    background-color: var(--bg-color);
+    border: 1px solid var(--border-color);
+
+    .section-title {
+      text-align: center;
+      color: var(--sec-font-color);
+      padding: 1.1rem 0;
+      border-bottom: 1px solid var(--border-color);
+      margin-top: 0;
+    }
+  }
+}
+</style>
