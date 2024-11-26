6f44dbcc680d20a5a680f218bf1ab9e201da2071
yasi-amani <yasamanamanii7020@gmail.com>
2024-11-26 18:22:41 +0330

    Update gaugechart

--- components/networkGauge.vue
+++ components/networkGauge.vue
@@ -10,17 +10,29 @@
       />
     </div>
     <div class="legend-container">
-      <div class="legend-item">
-        <span class="color-box" style="background-color: #ff6e76"></span>
-        <span>Pooled</span>
-        <span class="value">{{ pooledValue }}</span>
-      </div>
-      <div class="legend-item">
-        <span class="color-box" style="background-color: #a279ff"></span>
-        <span>Bonded</span>
-        <span class="value">{{ bondedValue }}</span>
-      </div>
+  <div v-if="!legendLoaded" class="skeleton-wrapper">
+    <div class="skeleton">
+    <div class="legend-item">
+      <skeleton-loader height="0.5rem" width="3rem" ></skeleton-loader>
     </div>
+    <div class="legend-item">
+      <skeleton-loader  height="0.5rem" width="3rem"></skeleton-loader>
+    </div>
+  </div>
+  </div>
+  <div v-else class="legend-content">
+    <div class="legend-item">
+      <span class="color-box" style="background-color: #07C86E"></span>
+      <span>Pooled</span>
+    </div>
+    <div class="legend-item">
+      <span class="color-box" style="background-color: #f3ba2f"></span>
+      <span>Bonded</span>
+    </div>
+  </div>
+</div>
+
+
   </Card>
 </template>
 
@@ -29,13 +41,14 @@ import { use } from 'echarts/core'
 import { SVGRenderer } from 'echarts/renderers'
 import { GaugeChart } from 'echarts/charts'
 import {
+  GraphicComponent,
   TitleComponent,
   TooltipComponent,
   GridComponent,
 } from 'echarts/components'
 import VChart from 'vue-echarts'
 
-use([SVGRenderer, GridComponent, GaugeChart, TitleComponent, TooltipComponent])
+use([SVGRenderer, GridComponent, GaugeChart, TitleComponent, TooltipComponent,GraphicComponent])
 
 export default {
   name: 'GaugeChart',
@@ -44,6 +57,7 @@ export default {
   },
   data() {
     return {
+      legendLoaded: false,
       loading: true,
       chartOption: null,
       pooledValue: 0,
@@ -52,70 +66,90 @@ export default {
   },
   mounted() {
     this.fetchData()
+    setTimeout(() => {
+    this.legendLoaded = true;
+  }, 2000);
   },
   methods: {
     async fetchData() {
-      try {
-        const { data } = await this.$api.getNetwork()
-        const pooled = data?.totalPooledRune / 1e8
-        const bonded = data?.bondMetrics?.totalActiveBond / 1e8
-        this.updateChartOption(pooled, bonded)
-        this.loading = false
-      } catch (error) {
-        console.error('Failed to fetch network data:', error)
-        this.loading = false
-      }
-    },
-    updateChartOption(pooled, bonded) {
-      const total = pooled + bonded
-      this.pooledValue = this.$options.filters.number(pooled, '0,0a')
-      this.bondedValue = this.$options.filters.number(bonded, '0,0a')
+  try {
+    const { data } = await this.$api.getNetwork();
 
-      this.chartOption = {
-        series: [
-          {
-            type: 'gauge',
-            startAngle: 180,
-            endAngle: 0,
-            min: 0,
-            max: total,
-            radius: '100%',
-            center: ['50%', '50%'],
-            axisLine: {
-              lineStyle: {
-                width: 50,
-                color: [
-                  [pooled / total, '#FF6E76'],
-                  [1, '#A279FF'],
-                ],
-              },
-            },
-            pointer: {
-              show: false,
-            },
-            axisTick: {
-              show: false,
-            },
-            splitLine: {
-              show: false,
-            },
-            axisLabel: {
-              color: 'var(--sec-font-color)',
-              fontSize: 10,
-              distance: -60,
-              formatter: function (value) {
-                if (value === pooled) {
-                  return 'POOLED'
-                } else if (value === bonded) {
-                  return 'BONDED'
-                }
-                return ''
-              },
-            },
+    console.log('totalPooledRune:', data?.totalPooledRune);
+    console.log('bondMetrics:', data?.bondMetrics);
+    const pooled = (data?.totalPooledRune ?? 0) / 1e8;
+    const bonded = (data?.bondMetrics?.totalActiveBond ?? 0) / 1e8;
+    this.updateChartOption(pooled, bonded);
+    this.loading = false;
+  } catch (error) {
+    console.error('Failed to fetch network data:', error);
+    this.loading = false;
+
+  }
+},
+updateChartOption(pooled, bonded) {
+  const total = pooled + bonded;
+
+  this.chartOption = {
+    series: [
+      {
+        type: 'gauge',
+        startAngle: 180,
+        endAngle: 0,
+        min: 0,
+        max: total,
+        radius: '100%',
+        center: ['50%', '50%'],
+        axisLine: {
+          lineStyle: {
+            width: 40,
+            color: [
+              [pooled / total, '#07C86E'],
+              [1, '#f3ba2f'],
+            ],
           },
-        ],
-      }
-    },
+        },
+        pointer: {
+          show: false, 
+        },
+        axisTick: {
+          show: false, 
+        },
+        splitLine: {
+          show: false, 
+        },
+        axisLabel: {
+          show: false, 
+        },
+        detail: {
+          show: false, 
+        },
+      },
+    ],
+    graphic: [
+      {
+        type: 'text',
+        left: '1%', 
+        top: '20%', 
+        style: {
+          text: `${this.$options.filters.number(pooled, '0,0a')}`, 
+          fill: '#07C86E', 
+          align: 'center',
+        },
+      },
+      {
+        type: 'text',
+        left: '88%', 
+        top: '15%', 
+        style: {
+          text: `${this.$options.filters.number(bonded, '0,0a')}`, 
+          fill: '#f3ba2f', 
+          align: 'center',
+        },
+      },
+    ],
+  };
+},
   },
 }
 </script>
@@ -147,11 +181,21 @@ export default {
   @include md {
     gap: 7rem;
   }
-
+  .skeleton{
+    display: flex;
+  }
+.legend-content{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+  gap: 12rem;
+}
   .legend-item {
     display: flex;
     align-items: center;
-    margin: 0 10px;
+    margin-top: -3rem;
+
 
     .color-box {
       width: 15px;
@@ -161,7 +205,6 @@ export default {
     }
 
     .value {
-      font-size: 14px;
       margin-left: 5px;
       color: var(--sec-font-color);
     }
