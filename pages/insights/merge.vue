e375c49fda7828c9cb6598e3a6af0f2f54df05e9
HooriRn <hoorirn@protonmail.com>
2025-04-05 18:39:24 +0330

    Add more info to RUJI merge page

--- pages/insights/merge.vue
+++ pages/insights/merge.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <Card style="margin-bottom: 1rem">
+      <span>
+        Holders of
+        <b style="color: var(--sec-font-color)"
+          >KUJI, rKUJI, FUZN, NSTK, WINK, and LVN</b
+        >
+        tokens are able to migrate and convert these tokens into RUJI on Rujira,
+        the THORChain app layer.
+      </span>
+      <br />
+      <br />
+      <a href="https://rujira.network/merge/KUJI" target="_blank">More Info</a>
+    </Card>
     <Card
       title="RUJIRA Merge"
       :img-src="require('@/assets/images/ruji-merge.png')"
@@ -28,15 +41,16 @@
             <AssetIcon :asset="props.row.Asset" />
             <span>{{ props.formattedRow[props.column.field] }}</span>
           </div>
-          <div
-            v-else-if="
-              props.column.field == 'MintE8' || props.column.field == 'BurnE8'
-            "
-          >
+          <div v-else-if="props.column.field == 'MintE8'">
             {{ props.formattedRow[props.column.field] }}
+            / {{ props.row.maxSupply | number('0,0.00a') }}
             <small>
               {{ showAsset(props.row.Asset, true) }}
             </small>
+          </div>
+          <div v-else-if="props.column.field == 'Allocation'">
+            {{ props.formattedRow[props.column.field] }}
+            <small>RUJI</small>
           </div>
           <span v-else>
             <span v-if="props.row[props.column.field]">
@@ -71,7 +85,14 @@ export default {
           tdClass: 'mono',
         },
         {
-          label: 'Count',
+          label: 'Allocation',
+          field: 'Allocation',
+          type: 'number',
+          formatFn: (n) => this.$options.filters.number(n, '0,0.00a'),
+          tdClass: 'mono',
+        },
+        {
+          label: 'Tx Count',
           field: 'Count',
           type: 'number',
           tdClass: 'mono',
@@ -95,6 +116,34 @@ export default {
       const mergeData = (await this.$api.getInfraRUJIMerge()).data
       mergeData.map((item) => {
         item.Asset = item.Asset.replace('THOR.', 'GAIA.')
+        switch (item.Asset) {
+          case 'GAIA.KUJI':
+            item.maxSupply = 122349807
+            item.Allocation = 41_840_000
+            break
+          case 'GAIA.WINK':
+            item.maxSupply = 69000000
+            item.Allocation = 1_430_000
+            break
+          case 'GAIA.RKUJI':
+            item.maxSupply = 10000000
+            item.Allocation = 3_720_000
+            break
+          case 'GAIA.FUZN':
+            item.maxSupply = 171740000
+            item.Allocation = 1_560_000
+            break
+          case 'GAIA.LVN':
+            item.maxSupply = 695260000
+            item.Allocation = 5_000_000
+            break
+          case 'GAIA.NSTK':
+            item.maxSupply = 65000000
+            item.Allocation = 1_440_000
+            break
+          default:
+            break
+        }
         return item
       })
       this.rows = mergeData
