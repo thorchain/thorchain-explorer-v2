32b665c0950801898c5b5ffedbb8889508100d41
HooriRn <hoorirn@protonmail.com>
2025-05-05 00:44:05 +0330

    Fix L1 swaps filter in txs

--- pages/txs/index.vue
+++ pages/txs/index.vue
@@ -80,7 +80,7 @@ export default {
         { label: 'All', filter: {} },
         {
           label: 'L1 Swaps',
-          filter: { type: ['swap'], asset: ['nosynth', 'notrade', 'norune'] },
+          filter: { type: ['swap'], asset: ['l1'] },
         },
         { label: 'Secure', filter: { type: ['secure'] } },
         { label: 'Trade Swaps', filter: { type: ['swap'], asset: ['trade'] } },
